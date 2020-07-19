159c05b7a0dc26b1eada0fbbd369810df41e91b6
Fabio Siqueira <fabiohfs85@gmail.com>
2020-07-18 21:25:52 -0300

    initial commit

---
+++ ProduceConsumerRabbitMQ.Producer/Sender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace ProduceConsumerRabbitMQ.Producer
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                string message = "Primeira mensagem do .Net Core para o RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(string.Empty, "BasicTest", null, body);
+                Console.WriteLine("Mensagem {0} enviada", message);
+            }
+
+            Console.WriteLine("Pressione enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
